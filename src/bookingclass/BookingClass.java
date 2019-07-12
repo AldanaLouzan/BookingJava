76523b0753301e8fa7257a3199eeff4f1b9a53c4
aldanaLouzan <aldana.louzan@gmail.com>
2019-07-12 15:57:27 +0100

    12/07 First attempt - packages: entities, controllers

--- src/bookingclass/BookingClass.java
+++ src/bookingclass/BookingClass.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bookingclass;
+
+import java.text.ParseException;
+import view.Menu;
 
 /**
  *
- * @author Eoin
+ * @author Aldana
  */
 public class BookingClass {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+        Menu m = new Menu ();
+
     }
     
 }
